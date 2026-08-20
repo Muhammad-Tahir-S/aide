8b312038ffcb8aacff19c030a77661633b49c0a2
Muhammad-Tahir <mtz5prif@gmail.com>
2026-08-20 08:32:12 +0100

    Remove unused icons, components, and theme provider; update package.json dependencies.

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,11 @@
 import AppRoutes from "./shared/app";
-import { ThemeProvider } from "./shared/components/theme-provider";
 import { AtmosphereProvider } from "./shared/theme/atmosphere-provider";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <AtmosphereProvider>
-        <AppRoutes />
-      </AtmosphereProvider>
-    </ThemeProvider>
+    <AtmosphereProvider>
+      <AppRoutes />
+    </AtmosphereProvider>
   );
 }
 
